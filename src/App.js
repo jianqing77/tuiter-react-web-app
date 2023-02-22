aa53c4d00607f4540c1ee95dda6a642592ded483
Jianiqng Ma <jianqing997@gmail.com>
2023-02-22 17:25:06 -0500

    a6 html 2023 spring - installed bootstrap library through npm, implemented Labs component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+// import Labs component
+import Labs from './labs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // returning an HTML tag, not an HTML string
+    // Reason: React.js uses a library called JSX or JavaScript XML
+    // JSX allows mixing and matching JavaScript and XML seamlessly
+    // and HTML is just a particular flavor of XML.
+    // Wrap the HTML content in a DIV element with the container class
+    return (
+        <div className="container">
+            <h1>Hello World!</h1>
+            <Labs /> {/* the Lab component */}
+        </div>
+    );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+//  imports the React and ReactDOM libraries.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import bootstrap related libraries
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+// React.js application is implemented in src/index.js
+// retrieve a reference to the DOM element declared in index.html using document.getElementById('root')
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM then creates an instance of App and appends its output to the element whose ID is root.
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
