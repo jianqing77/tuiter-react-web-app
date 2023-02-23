a1bb8036637f4e23cf4421bd15f8809b510527ae
Jianiqng Ma <jianqing997@gmail.com>
2023-02-22 19:00:53 -0500

    a6 html 2023SP - Lab - 2.7  use the Route component from React Router to declare paths

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import Labs from './labs';
 import HelloWorld from './labs/a6/hello-world';
 import Tuiter from './tuiter';
+// import browserRouter
+// BrowserRouter tag sets up the base mechanism to navigate between multiple components
+// to navigate between the three components within the BrowserRouter tag
+import { BrowserRouter } from 'react-router-dom';
+// To navigate between components we use the Route component from React Router to
+// declare paths and map them to corresponding component we want to render for that path
+import { Routes, Route } from 'react-router';
 
 function App() {
     // returning an HTML tag, not an HTML string
@@ -12,11 +19,16 @@ function App() {
     // and HTML is just a particular flavor of XML.
     // Wrap the HTML content in a DIV element with the container class
     return (
-        <div className="container">
-            <HelloWorld />
-            <Labs /> {/* the Lab component */}
-            <Tuiter />
-        </div>
+        // Add browerRouter tag
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/labs" element={<Labs />} />
+                    <Route path="/hello" element={<HelloWorld />} />
+                    <Route path="/tuiter" element={<Tuiter />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 export default App;
