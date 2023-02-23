b78719660bfbc15a1532b73aea690211560c3494
Jianiqng Ma <jianqing997@gmail.com>
2023-02-22 19:13:32 -0500

    a6 html 2023SP - Lab - 2.8 Navigating with links in SPAs

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function HelloWorld() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Link to="/">Lab</Link> |<Link to="/hello">Hello</Link> |<Link to="/tuiter">Tuiter</Link>
+            <h1>Hello World!</h1>
         </div>
     );
 }
-
 export default HelloWorld;
