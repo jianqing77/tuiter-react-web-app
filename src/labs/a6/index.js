fc3f03a22faad79c8a0b0d116a28bf760335ca52
Jianiqng Ma <jianqing997@gmail.com>
2023-02-22 22:49:12 -0500

    a6 html 2023SP - Lab - 2.13 Implemented ToDo List using React

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -2,12 +2,16 @@
 import Classes from './classes';
 import Styles from './styles';
 import ConditionalOutput from './conditional-output'; // import the conditional
+// import TodoItem from './todo/todo-item';
+import TodoList from './todo/todo-list';
 
 function Assignment6() {
     return (
         <div>
             {/* <Link to="/">Lab</Link> |<Link to="/hello">Hello</Link> |<Link to="/tuiter">Tuiter</Link> */}
             <h1>Assignment 6</h1>
+            {/* <TodoItem /> */}
+            <TodoList />
             <ConditionalOutput />
             <Styles />
             <Classes />
