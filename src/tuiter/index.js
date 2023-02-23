d0e80cfd333cb0e020832d0b2d457a2850fbfc1e
Jianiqng Ma <jianqing997@gmail.com>
2023-02-23 11:28:53 -0500

    a6 html 2023SP - Lab - 3.1.5 Render dynamic classes with embedded expressions

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,13 @@
 // import { Link } from 'react-router-dom';
 import Nav from '../nav';
-
+import NavigationSidebar from './navigation-sidebar';
 function Tuiter() {
     return (
         <div>
             {/* add navigation links */}
             {/* <Link to="/">Lab</Link> |<Link to="/hello">Hello</Link> |<Link to="/tuiter">Tuiter</Link> */}
             <Nav />
+            <NavigationSidebar active="home" />
             <h1>Tuiter</h1>
         </div>
     );
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -11,14 +11,46 @@ const NavigationSidebar = ({
         // use classname instead of class
         <div className="list-group">
             <a className="list-group-item">Tuiter</a>
-            <a className="list-group-item">Home</a>
-            <a className="list-group-item">Explore</a>
-            <a className="list-group-item">Notifications</a>
-            <a className="list-group-item">Messages</a>
-            <a className="list-group-item">Bookmarks</a>
-            <a className="list-group-item">Lists</a>
-            <a className="list-group-item">Profile</a>
-            <a className="list-group-item">More</a>
+            <a
+                className={`list-group-item
+                    ${active === 'home' ? 'active' : ''}`}>
+                Home
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'explore' ? 'active' : ''}`}>
+                Explore
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'notifications' ? 'active' : ''}`}>
+                Notifications
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'messages' ? 'active' : ''}`}>
+                Messages
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'bookmarks' ? 'active' : ''}`}>
+                Bookmarks
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'lists' ? 'active' : ''}`}>
+                Lists
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'profile' ? 'active' : ''}`}>
+                Profile
+            </a>
+            <a
+                className={`list-group-item
+                    ${active === 'more' ? 'active' : ''}`}>
+                More
+            </a>
         </div>
     );
 };
