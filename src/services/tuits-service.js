f901a76dd4e350389f8e16a62393cdde7455f842
Jianiqng Ma <jianqing997@gmail.com>
2023-04-08 20:46:09 -0400

    a9 Mongo sp23

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 // async tags functions as asynchronous
+console.log('TUITS_API');
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit);
